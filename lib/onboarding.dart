91a1fd807c3c444ac15f83309fa3c16fcb39bf18
BillalEmon <billal.emon@northsouth.edu>
2022-05-27 21:30:54 +0600

    started working on today's part and initially changed some style of on boarding

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -10,16 +10,25 @@ class OnboardingPage extends StatelessWidget {
       body: PageView.builder(
           itemCount: controller.onboardingPages.length,
           itemBuilder: (context,index){
-        return Container(
-          child: Column(
-            children: [
-              Image.asset(controller.onboardingPages[index].images),
-              Text(controller.onboardingPages[index].titles),
-              Text(controller.onboardingPages[index].descriptions),
-            ],
-          ),
-        );
-      }),
+            return Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(controller.onboardingPages[index].images),
+                  SizedBox(height: 35),
+                  Text(controller.onboardingPages[index].titles, style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),),
+                  SizedBox(height: 35),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text(
+                      controller.onboardingPages[index].descriptions,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),),
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
